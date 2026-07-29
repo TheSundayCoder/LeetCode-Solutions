3af1bd56e007554fd2fc95d5feae8ed98c0c3bc8
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-07-29 22:37:47 +0530

    Correct smallestEvenMultiple function logic
    Fix loop condition and remove unnecessary exit call.

--- 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
+++ 2413-smallest-even-multiple/2413-smallest-even-multiple.cpp
@@ -3,8 +3,7 @@ public:
     int smallestEvenMultiple(int n) {
         for(int i = 1 ; 3<4 ; i++){
              if(i%2==0 && i %n==0){
-                return i;
-                exit(0);
+                return i;//loop ends here
                 
              }
         }
