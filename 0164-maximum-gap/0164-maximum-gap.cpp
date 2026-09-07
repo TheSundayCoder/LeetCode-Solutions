b7f92f1047c2621917deb9cdc7b8ab1ae382302a
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-09-07 14:43:44 +0530

    Time: 52 ms (14.8%), Space: 85.9 MB (74.88%) - LeetHub

---
+++ 0164-maximum-gap/0164-maximum-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int maximumGap(vector<int>& nums) {
+        sort(nums.begin() , nums.end());int max = INT_MIN;
+        if(nums.size()==1){
+            return 0;
+        }
+        for(int i = 0 ; i < nums.size()-1; i++){
+            if(nums[i+1] - nums[i] > max){
+                max = nums[i+1] - nums[i];
+            }
+        } 
+        return max;
+    }
+};
