08b8aaf353dabe94180f2c91d63d6deadcac9b27
Aditya Sinha <sinhaaditya5000@gmail.com>
2026-09-08 19:39:58 +0530

    Time: 0 ms (100%), Space: 8.6 MB (48.57%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -3,11 +3,7 @@ public:
     int gcdOfOddEvenSums(int n) {
         int so = n*(n+1),max =0 ;
         int se = n*n;
-        for(int i = 1 ; i <= so ; i++){
-            if(so%i == se%i){
-                  max = i;
-            }
-        }
+        max = gcd(so,se);
         return max;
     }
 };
